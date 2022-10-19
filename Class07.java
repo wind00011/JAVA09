40f7d5c9cc22547c1c6f37ff626b8cdfde3d7de8
wind00011 <113489040+wind00011@users.noreply.github.com>
2022-10-19 23:32:10 +0800

    Add files via upload

---
+++ Class07.java
@@ -0,0 +1,18 @@
+package Java09;
+
+class Count {
+	public static void power(int x, int n)
+
+	{
+		System.out.println(Math.pow(x, n)); 
+	}
+
+}
+
+public class Class07 {
+	public static void main(String args[]) {
+		Count.power(2, 5);
+		Count.power(3, 2);
+	}
+
+}
